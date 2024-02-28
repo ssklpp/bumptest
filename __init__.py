e7ef96337694d42dcd1df4352827d9a11bc1fb95
Kim taewan <ssklpp@naver.com>
2024-02-28 17:33:40 +0900

    14

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
-__version__ == '2.1.5'
+# __version__ == '2.1.6'
+
+from bump2version import *
+
+current_version = get_version()
+new_version = bump_version(current_version)
+
+# 패키지 버전 정보 업데이트
+set_version(new_version)
+
+# 버전 변경 커밋 및 태그 생성
+if __name__ == '__main__' :
+    print(f"Bumping version from {current_version} to {new_version}")
+    commit_changes()
+    tag_version()
