2572f598a7ce665b4a0ec09e03e54451082d5845
Kim taewan <ssklpp@naver.com>
2024-02-29 09:57:18 +0900

    testtest

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-# __version__ == '2.1.7'
+# __version__ == '2.2.0'
 
-from bump2version import *
+from bumpversion import *
 
 current_version = get_version()
 new_version = bump_version(current_version)
@@ -9,7 +9,7 @@ new_version = bump_version(current_version)
 set_version(new_version)
 
 # 버전 변경 커밋 및 태그 생성
-if __name__ == '__main__' :
-    print(f"Bumping version from {current_version} to {new_version}")
+if __name__ == '__main__':
+    print('Bumping version from {} to {}'.format(current_version, new_version))
     commit_changes()
     tag_version()
